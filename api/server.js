1b24264c338b599def81522bf18f04d096a5046c
jyp0426 <3394521327@qq.com>
2022-04-21 18:58:46 +0800

    Updated

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 80;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+});
---
+++ js/15-回调地狱&Promise对象&async函数.js
@@ -0,0 +1,8 @@
+// 回调地狱：多个回调函数之间进行嵌套时会产生回调地狱，导致代码难以维护
+// Promise对象和async函数的出现就是为了解决回调地狱的问题
+// Promise对象的使用方法
+// 异步方法并不会立即返回最终的值，而是会返回一个 Promise，以便在未来某个时候把值交给调用者
+// 一个Promise一共有三种状态：pending（初始状态）、fulfilled（操作成功）、rejected（操作失败），其中，pending可以转化为fulfilled或rejected状态
+
+
+// async函数的使用方法
