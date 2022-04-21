57e6898d02e4442448328245d4463f4ee94f1281
jyp0426 <3394521327@qq.com>
2022-04-21 19:23:04 +0800

    Updated

--- js/15-回调地狱&Promise对象&async函数.js
+++ js/15-回调地狱&Promise对象&async函数.js
@@ -3,6 +3,11 @@
 // Promise对象的使用方法
 // 异步方法并不会立即返回最终的值，而是会返回一个 Promise，以便在未来某个时候把值交给调用者
 // 一个Promise一共有三种状态：pending（初始状态）、fulfilled（操作成功）、rejected（操作失败），其中，pending可以转化为fulfilled或rejected状态
-
+url = 'http://localhost'
+function getIndexPage(url) {
+    return new Promise((resolve, reject) => {
+        
+    });
+}
 
 // async函数的使用方法
